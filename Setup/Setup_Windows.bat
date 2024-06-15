4159c2fd680ec685c646b9c9881be327ea876f91
cpluspluslowlevel <55049810+cpluspluslowlevel@users.noreply.github.com>
2024-06-15 12:27:11 +0900

    vcpkg관련 구현 모두 취소
    vcpkg를 이용해 clang, ninja, gdb를 설치하는 기능을 제공하려 했으나 지원하지 않아서 취소. ChatGPT의 할루시네이션인듯...
    관련 툴들을 설치해주는 기능은 일단 보류하기로 결정.

--- Setup/Setup_Windows.bat
+++ Setup/Setup_Windows.bat
@@ -2,7 +2,9 @@
 setlocal
 
 set EXTERNAL_DIRECTORY=%~dp0\..\External
-set VCPKG_DIRECTORY=%EXTERNAL_DIRECTORY%\vcpkg
+
+echo -------------------------------------------------------------------------------
+echo Checking directory
 
 echo Checking %EXTERNAL_DIRECTORY% directory
 if not exist "%EXTERNAL_DIRECTORY%" (
@@ -12,47 +14,7 @@ if not exist "%EXTERNAL_DIRECTORY%" (
     echo %EXTERNAL_DIRECTORY% directory already exists
 )
 
-echo Checking %VCPKG_DIRECTORY% directory
-if not exist "%VCPKG_DIRECTORY%" (
-    echo Creating %VCPKG_DIRECTORY% directory
-    mkdir "%VCPKG_DIRECTORY%"
-) else (
-    echo %VCPKG_DIRECTORY% directory already exists
-)
-
-echo Checking vcpkg
-%VCPKG_DIRECTORY%\vcpkg.exe --version
-echo Error code: %errorlevel%
-if %errorlevel% equ 0 (
-    echo vcpkg already installed.
-) else (
-
-    echo Installing vcpkg
-
-    echo Cloning vcpkg repository
-    git clone https://github.com/microsoft/vcpkg.git "%VCPKG_DIRECTORY%"
-    echo Error code: %errorlevel%
-
-    echo Bootstrapping vcpkg
-    call %VCPKG_DIRECTORY%\bootstrap-vcpkg.bat
-    echo Error code: %errorlevel%
-
-)
-
-
-
-echo Installing packages
-echo Checking vcpkg
-%VCPKG_DIRECTORY%\vcpkg.exe --version
-echo Error code: %errorlevel%
-if %errorlevel% equ 0 (
-    echo vcpkg installed.
-) else (
-    echo Failed to install vcpkg.
-    exit /b 1
-)
-
-echo Installing clang
+echo -------------------------------------------------------------------------------
 
 
 endlocal
